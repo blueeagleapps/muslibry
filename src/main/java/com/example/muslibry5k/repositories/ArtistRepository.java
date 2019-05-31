c810852dd8ed4e0f578706ec6167181dce00f1e5
Blue Eagle Apps <pk@pksroczynski.com>
2019-05-31 17:55:42 +0200

    modified: - songs.html (bootstrap added) - artists.html (bootstrap added) - data.sql added for inflate db with more data

--- src/main/java/com/example/muslibry5k/repositories/ArtistRepository.java
+++ src/main/java/com/example/muslibry5k/repositories/ArtistRepository.java
@@ -4,4 +4,5 @@ import com.example.muslibry5k.model.Artist;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ArtistRepository extends CrudRepository<Artist, Long> {
+
 }
