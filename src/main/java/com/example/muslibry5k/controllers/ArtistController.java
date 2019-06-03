48a25c103e1603e7007ab05678b3070e473e2afa
Cheeyenne <piotr.sroczynski@gmail.com>
2019-06-03 16:26:09 +0200

    added: - new ArtistController method: getArtistSongs - SongRepository new method for getting all songs for an artist
    modified:
    - artist/list template: new action added (Songs)
    - song/list template: now list is more universal (new attribute supported for filtered list :filter; is null song list look like before

--- src/main/java/com/example/muslibry5k/controllers/ArtistController.java
+++ src/main/java/com/example/muslibry5k/controllers/ArtistController.java
@@ -1,24 +1,46 @@
 package com.example.muslibry5k.controllers;
 
+import com.example.muslibry5k.model.Artist;
 import com.example.muslibry5k.repositories.ArtistRepository;
+import com.example.muslibry5k.repositories.SongRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.Optional;
+
 @Controller
 public class ArtistController {
 
     private ArtistRepository artistRepository;
+    private SongRepository songRepository;
 
-    public ArtistController(ArtistRepository artistRepository) {
+    public ArtistController(ArtistRepository artistRepository, SongRepository songRepository) {
         this.artistRepository = artistRepository;
+        this.songRepository = songRepository;
     }
 
     @RequestMapping(value = {"/artists", "/artist/list"})
     public String getArtists(Model model) {
         model.addAttribute("artists", artistRepository.findAll());
         return "artist/list";
+    }
+
+    @RequestMapping("/artist/{id}/songs")
+    public String getArtistSongs(Model model, @PathVariable("id") Long id) {
+        Optional<Artist> artist = artistRepository.findById(id);
+
+        if (artist.isPresent()) {
+            model.addAttribute("songs", songRepository.getAllByArtistsIsContaining(artist.get()));
+            model.addAttribute("filter", "artist: " + artist.get().getFirstName() + " " + artist.get().getLastName());
+        } else {
+            model.addAttribute("songs", new ArrayList<>());
+            model.addAttribute("filter", "artist for this id doesn't exist");
+        }
+
+        return "song/list";
     }
 
     @RequestMapping("/artist/{id}/show")
--- src/main/java/com/example/muslibry5k/repositories/SongRepository.java
+++ src/main/java/com/example/muslibry5k/repositories/SongRepository.java
@@ -1,7 +1,12 @@
 package com.example.muslibry5k.repositories;
 
+import com.example.muslibry5k.model.Artist;
 import com.example.muslibry5k.model.Song;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface SongRepository extends CrudRepository<Song, Long> {
+
+    List<Song> getAllByArtistsIsContaining(Artist artist);
 }
